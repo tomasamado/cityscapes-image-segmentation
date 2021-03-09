65269541cee034e69afd3c93470f7dc076819923
Pablo Valdunciel <pava00001@lofn.cs.uni-saarland.de>
2021-03-09 09:39:51 +0100

    EvaluationReport finished (from_model() to be checked)

--- evaluation.py
+++ evaluation.py
@@ -2,81 +2,219 @@
     evaluation module 
 """
 
-import sklearn.metrics as sk_metrics
+import torch
+import numpy as np
+from sklearn.metrics import multilabel_confusion_matrix as ml_conf_m
 
 
-class BinaryEvaluation(Object):
-    """ Binary Evaluation """
+class EvaluationReport:
+    """ Evaluation report for a multiclass pixel-level segmentation task """
     
-    def __init__(self, confusion_matrix):
+    def __init__(self, confusion_matrix, labels):
         self.cm = confusion_matrix        
+        self.labels = np.array(labels)
+        self.n_classes = len(labels)
+        self.decimal_places = 4
+
     
-    
-    # Factory methods 
+    # Factory methods     
     @classmethod
-    def from_bin_conf_matrix(confusion_matrix):
-        """ Create a BinaryEvaluation object given the confussion matrix
-        
-            Parameters:
-                confusion_matrix (np.array) - Binary confusion matrix"""
-        return cls(confusion_matrix)
-    
-    @classmethod
-    def from_multilabel_predictions(y_true, y_pred, target_class):
-        """ Create a BinaryEvaluation object given the groud-truth labels and the 
+    def from_predictions(cls, y_true, y_pred, labels):
+        """ Create a MetricsReport object given the ground-truth labels and the 
             predicted labels. 
         
             Parameters:
                 y_true (array-like of shape) - Ground truth (correct) label values
                 y_pred (array-like of shape) - Predicted label values
-                target_class (int) - the positive class to compare vs. the rest 
+                labels (array-like of shape) - Possible pixel labels 
 
             Returs:
-                (np.array) - Confusion matrix 2x2
+                (EvaluationReport) - A evaluation report
         """
         true_list = torch.zeros(0, dtype=torch.long, device='cpu')
         pred_list = torch.zeros(0, dtype=torch.long, device='cpu')
 
         for i in range(len(y_true)):
-            true_list = torch.cat([true_list, 
-                                   (y_true[i] == target_class).long().view(-1).cpu()])
-            pred_list = torch.cat([pred_list, 
-                                   (y_pred[i] == target_class).long().view(-1).cpu()])    
-        cm = sk_metrics.confusion_matrix(true_list.numpy(), pred_list.numpy())
-        return cls(cm)
+            true_list = torch.cat([true_list, y_true[i].view(-1).cpu()])
+            pred_list = torch.cat([pred_list, y_pred[i].view(-1).cpu()])    
+        cm = ml_conf_m(true_list.numpy(), pred_list.numpy(), labels=labels)
+        return cls(cm, labels)
+
+    @classmethod
+    def from_model(cls, dataloader, model, labels):
+        """ Create a MetricsReport object given a dataloader and the model to
+             make the predictions.
+
+            Parameters:
+                dataloader (torch.utils.data.Dataloader) - Dataloader
+                model (torch.nn.Module) - Model to make the predictions
+                labels (array-like of shape) - Possible pixel labels
+
+            Returs:
+                (EvaluationReport) - A evaluation report
+        """
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        true_list = torch.zeros(0, dtype=torch.long, device='cpu')
+        pred_list = torch.zeros(0, dtype=torch.long, device='cpu')
+        model = model.to(device)
+
+        with torch.no_grad():
+            for inputs, labels in dataloader:
+                inputs = inputs.to(device)
+                outputs = model(x)
+                _, preds = torch.max(outputs, 1)
+                true_list = torch.cat([true_list, labels.view(-1).cpu()])
+                pred_list = torch.cat([pred_list, preds.view(-1).cpu()])  
+
+        cm = ml_conf_m(true_list.numpy(), pred_list.numpy(), labels=labels)
+        return cls(cm, labels)
     
     @classmethod
-    def from_model(y_true, model, target_class):
-        return None
+    def from_model(cls, inputs, y_true, model, labels):
+        """ Create a MetricsReport object given the ground-truth labels and the 
+            predicted labels. 
+        
+            Parameters:
+                inputs (array-like of shape) - Inputs
+                y_true (array-like of shape) - Ground truth (correct) label values
+                model (torch.nn.Module) - Model to make the predictions
+                labels (array-like of shape) - Possible pixel labels
+
+            Returs:
+                (EvaluationReport) - A evaluation report
+        """
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        true_list = torch.zeros(0, dtype=torch.long, device='cpu')
+        pred_list = torch.zeros(0, dtype=torch.long, device='cpu')
+        model = model.to(device)
+        
+        with torch.no_grad():
+            for i, x in enumerate(inputs):
+                x = x.to(device)
+                output = model(x)
+                _, pred = torch.max(output, 1)
+                true_list = torch.cat([true_list, y_true[i].view(-1).cpu()])
+                pred_list = torch.cat([pred_list, pred.view(-1).cpu()])  
+                
+        cm = ml_conf_m(true_list.numpy(), pred_list.numpy(), labels=labels)
+        return cls(cm, labels)
     
     
     # Evaluation metrics 
-    def accuracy(self):
-        """ Compute the accuracy """
-        TN, FP, FN, TP = self.cm.ravel()
-        return ((TP + TN) / (TP + TN + FP + FN))
+    def all_metrics(self, target_class): 
+         """ Compute all metrics for a target class in a 
+            one vs. rest approach.
+            
+            Parameters:
+                target_class (int) - Target class
+                
+            Returns: 
+                (float) - Accuracy
+        """
+        return {
+            'accuracy': self.accuracy(target_class),
+            'sensitivity': self.sensitivity(target_class),
+            'specificity': self.specificity(target_class),
+            'dice_coeff': self.dice_coeff(target_class),
+            'jaccard_sim': self.jaccard_similarity(target_class),
+            'f1_score': self.f1_score(target_class)
+        }
+        
+    def accuracy(self, target_class):
+        """ Compute the accuracy for a target class in a 
+            one vs. rest approach.
+            
+            Parameters:
+                target_class (int) - Target class
+                
+            Returns: 
+                (float) - Accuracy
+        """
+        assert any(np.isin(self.labels, target_class)), "unknown target class"
+        index = np.where(self.labels == target_class)[0][0]
+        TN, FP, FN, TP = self.cm[index].ravel()
+        acc = (TP + TN) / (TP + TN + FP + FN)
+        return round(acc, self.decimal_places)
     
-    def sensitivity(self):
-        """ Compute the sensitivity """
-        TN, FP, FN, TP = self.cm.ravel()
-        return (TP / (TP + FN))
+    def sensitivity(self, target_class):
+        """ Compute the sensitivity for a target class in a 
+            one vs. rest approach.
+            
+            Parameters:
+                target_class (int) - Target class
+                
+            Returns: 
+                (float) - Accuracy
+        """
+        assert any(np.isin(self.labels, target_class)), "unknown target class"
+        index = np.where(self.labels == target_class)[0][0]
+        TN, FP, FN, TP = self.cm[index].ravel()
+        sens = (TP / (TP + FN))
+        return round(sens, self.decimal_places)
     
-    def specificity(self):
-        """ Compute the specificity """
-        TN, FP, FN, TP = self.cm.ravel()
-        return (TN / (TN+FP))
     
-    def dice_coeff(self):
-        """ Compute Dice Coefficient """
-        TN, FP, FN, TP = self.cm.ravel()
-        return (2*TP / (2*TP + FP + FN))
+    def specificity(self, target_class):
+        """ Compute the specificity for a target class in a 
+            one vs. rest approach.
+            
+            Parameters:
+                target_class (int) - Target class
+                
+            Returns: 
+                (float) - Accuracy
+        """
+        assert any(np.isin(self.labels, target_class)), "unknown target class"
+        index = np.where(self.labels == target_class)[0][0]
+        TN, FP, FN, TP = self.cm[index].ravel()
+        spec = (TN / (TN+FP))
+        return round(spec, self.decimal_places)
     
-    def jaccard_similarity(self):
-        """ Compute Jaccard Similarity """
-        TN, FP, FN, TP = self.cm.ravel()
-        return (TP / (TP + FP + FN))
     
-    def f1_score(self):
-        """ Compute the F1-score """
-        TN, FP, FN, TP = self.cm.ravel()
-        return (TP / (TP + 0.5*(FP + FN)))
+    def dice_coeff(self, target_class):
+        """ Compute the Dice's coefficient for a target class in a 
+            one vs. rest approach.
+            
+            Parameters:
+                target_class (int) - Target class
+                
+            Returns: 
+                (float) - Accuracy
+        """
+        assert any(np.isin(self.labels, target_class)), "unknown target class"
+        index = np.where(self.labels == target_class)[0][0]
+        TN, FP, FN, TP = self.cm[index].ravel()
+        dc =  (2*TP / (2*TP + FP + FN))
+        return round(dc, self.decimal_places)
+    
+    def jaccard_similarity(self, target_class):
+        """ Compute the Jaccards Similarity for a target class in a 
+            one vs. rest approach.
+            
+            Parameters:
+                target_class (int) - Target class
+                
+            Returns: 
+                (float) - Accuracy
+        """
+        assert any(np.isin(self.labels, target_class)), "unknown target class"
+        index = np.where(self.labels == target_class)[0][0]
+        TN, FP, FN, TP = self.cm[index].ravel()
+        js = (TP / (TP + FP + FN))
+        return round(js, self.decimal_places)
+    
+    def f1_score(self, target_class):
+        """ Compute the F1-score for a target class in a 
+            one vs. rest approach.
+            
+            Parameters:
+                target_class (int) - Target class
+                
+            Returns: 
+                (float) - Accuracy
+        """
+        assert any(np.isin(self.labels, target_class)), "unknown target class"
+        index = np.where(self.labels == target_class)[0][0]
+        TN, FP, FN, TP = self.cm[index].ravel()
+        f1 =  (TP / (TP + 0.5*(FP + FN)))
+        return round(f1, self.decimal_places)
+    
