bed01e7fa7158641eb5a55660163b5d1f306c70d
Tomas Amado <tomasamadopeters@lofn.cs.uni-saarland.de>
2021-03-11 21:10:04 +0100

    implemented rrcn block and r2unet arch

---
+++ r2unet.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[379]:
+
+
+import os
+import torch.nn as nn
+import torch.nn.functional as F
+import torch   
+import rrcnblock
+import matplotlib.pyplot as plt
+
+class Downsample(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(Downsample, self).__init__()
+        
+        self.rrcu = rrcnblock.RRConv(in_channel, out_channel)
+        self.maxpool = nn.MaxPool2d(2)
+        
+    def forward(self, x):
+        
+        x = self.rrcu(x)
+        x_up = x
+        x = self.maxpool(x)
+        
+        return x, x_up
+    
+class Upsample(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(Upsample, self).__init__()
+        
+        self.convtrans1 = nn.ConvTranspose2d(in_channel, out_channel, 3, stride = 2, padding = 1, output_padding = 1)
+        self.rrcu = rrcnblock.RRConv(in_channel, out_channel)
+        
+    def forward(self, x, x_crop):
+        
+        x = F.relu(self.convtrans1(x))
+        x = torch.cat((x_crop, x), dim = 1)
+        x = self.rrcu(x)
+           
+        return x
+    
+class R2UNet(nn.Module):
+    def __init__(self):
+        super(R2UNet, self).__init__()
+        
+        #Conv layer
+        
+        self.downsample1 = Downsample(3,64)
+        self.downsample2 = Downsample(64,128)
+        self.downsample3 = Downsample(128,256)
+        self.downsample4 = Downsample(256,512)
+        self.downsample5 = Downsample(512,1024)
+        
+        self.upsample1 = Upsample(1024,512)
+        self.upsample2 = Upsample(512,256)
+        self.upsample3 = Upsample(256,128)
+        self.upsample4 = Upsample(128,64)     
+        
+        self.rrcu1 = rrcnblock.RRConv(512, 1024)
+        self.conv1 = nn.Conv2d(64, 19, 1)
+
+        
+
+    def forward(self, x):
+
+        x, x_crop1 = self.downsample1(x) 
+        x, x_crop2 = self.downsample2(x)   
+        x, x_crop3 = self.downsample3(x)    
+        x, x_crop4 = self.downsample4(x)
+        
+        x = self.rrcu1(x)
+        
+        x = self.upsample1(x, x_crop4)
+        x = self.upsample2(x, x_crop3)
+        x = self.upsample3(x, x_crop2)
+        x = self.upsample4(x, x_crop1)
+        
+        x = F.relu(self.conv1(x))
+        
+        return x
+
---
+++ rrcnblock.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+# %%
+import os
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class RConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(RConv, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 1, padding = 1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, 1, padding = 1)
+        
+    def forward(self, x):
+        
+        x_f = self.conv1(x)        
+        h1 = self.conv2(x_f)   
+        h2 = self.conv2(h1) + x_f 
+        h3 = self.conv2(h2) + x_f      
+        
+        return h3
+    
+class RRConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(RRConv, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, 1, padding = 1)
+        self.rconv1 = RConv(in_channels, out_channels)
+        self.rconv2 = RConv(out_channels, out_channels)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        
+        #batch norm
+    
+    def forward(self, x):       
+        iden = self.conv1(x)
+        x = F.relu(self.bn1(self.rconv1(x)))
+        x = F.relu(self.bn1(self.rconv2(x)))
+        
+        x = x + iden
+        x = F.relu(x)
+        
+        return x       
+
