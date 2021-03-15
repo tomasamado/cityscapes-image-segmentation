e578175778855cb6ac746ef54927aaabe57c0d86
Pablo Valdunciel <pava00001@stud.uni-saarland.de>
2021-03-15 18:22:29 +0100

    normal_init added

--- utils.py
+++ utils.py
@@ -6,7 +6,25 @@ import torch
 import numpy as np 
 import matplotlib.pyplot as plt
 
-# +
+def normal_init(m, mean=0, std=1):
+    """Initialize the weights of a module using a normal distribution 
+
+    Args:
+        m (nn.Module) - Module to be initialize
+        mean (number) - Mean of the normal distribution.
+        std (number) - Standard deviation of the normal distribution.
+
+    Ref: https://discuss.pytorch.org/t/pytorch-how-to-initialize-weights/81511/2
+    """
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        m.weight.data.normal_(mean, std)
+        if m.bias.data is not None:
+            m.bias.data.zero_()
+    elif isinstance(m, (nn.BatchNorm2d, nn.BatchNorm1d)):
+        m.weight.data.fill_(1)
+        if m.bias.data is not None:
+            m.bias.data.zero_()
+
 def median_frequency_balancing(training_data, n_classes):
     """ The weight of class c  is computed as weight(c) = median freq/freq(c) where 
         freq(c) is the number of pixels of class c divided by the total number of 
@@ -117,6 +135,5 @@ def plot_metric(metric_history, label):
     plt.ylabel(label)
     plt.legend()
     plt.show()
-# -
 
 
